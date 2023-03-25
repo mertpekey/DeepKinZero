24961675b6cd6e4bb47a8ec48800e2ca84e5730d
Mert Pekey <mpekey@gmail.com>
2023-03-25 14:10:22 +0300

    All configs added

--- config.py
+++ config.py
@@ -5,6 +5,14 @@ KINASE_PATH = os.path.join(DATA_PATH,"AllKinases.txt")
 KINASE_EMBEDDING_PATH = os.path.join(DATA_PATH,"KinaseFeatures.txt")
 AMINOACIDS = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '_']
 SEQ_SIZE = 7 # Sequence size of phosphosite s + 1 + s
-BATCH_SIZE = 64 # dummy
+BATCH_SIZE = 64
 LEARNING_RATE = 0.001
 CLIP_GRADIENTS = 9.0
+RNN_UNIT_TYPE = 'LNlstm'
+NUM_LSTM_LAYERS = 2
+NUM_HIDDEN_UNITS = 512
+DROPOUT_VAL = 0.5
+EMBEDDING_DIM: 500
+ATTENTION_SIZE: 20
+REGS = 0.001
+NUM_OF_MODELS = 10
