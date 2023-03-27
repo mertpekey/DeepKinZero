37d04aa2fa05921bc44bc2689f41f39cd37b552c
Mert Pekey <mpekey@gmail.com>
2023-03-28 00:39:05 +0300

    Final configs added with dataset paths. Most of them not necessary and will be deleted later

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ VAL_KINASE_CANDIDATES = 'Dataset/Val_Candidate_Kinases.txt'
 TEST_DATA = 'Dataset/PhosPhoELM/PhoELMdata.txt'
 TEST_KINASE_CANDIDATES = 'Dataset/AllCandidates.txt'
 
-DATA_PATH = '/Dataset'
-KINASE_PATH = os.path.join(DATA_PATH,"AllKinases.txt")
-KINASE_EMBEDDING_PATH = os.path.join(DATA_PATH,"KinaseFeatures.txt")
+DATA_PATH = '/Users/mpekey/Desktop/Thesis/DeepKinZero/Dataset/'
+KINASE_PATH = '/Users/mpekey/Desktop/Thesis/DeepKinZero/Dataset/KinaseFeatures.txt'
+KINASE_EMBEDDING_PATH = '/Users/mpekey/Desktop/Thesis/DeepKinZero/Dataset/KinaseFeatures.txt'
 AMINOACIDS = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '_']
 SEQ_SIZE = 7 # Sequence size of phosphosite s + 1 + s
 BATCH_SIZE = 64
@@ -18,7 +18,7 @@ RNN_UNIT_TYPE = 'LNlstm'
 NUM_LSTM_LAYERS = 2
 NUM_HIDDEN_UNITS = 512
 DROPOUT_VAL = 0.5
-EMBEDDING_DIM: 500
-ATTENTION_SIZE: 20
+EMBEDDING_DIM= 500
+ATTENTION_SIZE= 20
 REGS = 0.001
 NUM_OF_MODELS = 10
