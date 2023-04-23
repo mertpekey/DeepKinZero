78aa94115e264795844c0bd65557b9fd71982c14
Mert Pekey <mpekey@gmail.com>
2023-04-23 20:55:05 +0300

    Test evaluation added. Loaded model and scaler planned to be added

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-
+from create_dataset import create_datasets
+from model import softmax
+from utils import ensemble, get_eval_predictions
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import MultiLabelBinarizer
+import torch
+import numpy as np
 
 def test_model(args):
     pass
@@ -6,10 +12,64 @@ def test_model(args):
 
 
     # Results (Test)
-    #if config.TEST_DATA != '':
-    #    for trainer in trainers:
-    #       TestUniProtIDs, test_probabilities = trainer.predict(test_dataset.DE, test_candidate_kinase_embeddings, test_candidate_ke_to_kinase)
-    #        TestUniProtIDs, test_probabilities = ensemble(TestUniProtIDs, test_probabilities, test_candidate_uniprotid)
-    #        Test_Evaluation = GetAccuracyMultiLabel(TestUniProtIDs, test_probabilities, test_kinase_uniprotid, test_dataset.TCI)
-    #        print('Acccuracy_Test: {}  Loss_Test: {} Top3Accuracy: {} Top5Accuracy: {} Top10Accuracy: {}'.format(Test_Evaluation["Accuracy"], Test_Evaluation["Loss"], Test_Evaluation["Top3Acc"], Test_Evaluation["Top5Acc"], Test_Evaluation["Top10Acc"]))
-            
+    if args.TEST_DATA != '':
+
+        # Load Models
+        loaded_models = []
+
+        # Load Embed Scaler
+        embed_scaler = None
+       
+        test_dataset, _, KE, test_candidate_kinase_embeddings, test_candidate_ke_to_kinase, test_kinase_uniprotid, test_candidate_uniprotid = create_datasets(args.TEST_DATA, args.TEST_KINASE_CANDIDATES, mode='test',embed_scaler=embed_scaler)
+
+        mlb_test = MultiLabelBinarizer()
+        binlabels_true_test = mlb_test.fit_transform(test_kinase_uniprotid)
+
+        for model in loaded_models:
+            TestUniProtIDs, test_probabilities = eval_test(test_dataset.DE, test_candidate_kinase_embeddings, test_candidate_ke_to_kinase)
+            TestUniProtIDs, test_probabilities = ensemble(TestUniProtIDs, test_probabilities, test_candidate_uniprotid)
+            Test_Evaluation, binlabels_pred = get_eval_predictions(TestUniProtIDs, test_probabilities, test_kinase_uniprotid, test_dataset.TCI, mlb_test)
+            print('Acccuracy_Test: {}  Loss_Test: {} Top3Accuracy: {} Top5Accuracy: {} Top10Accuracy: {}'.format(Test_Evaluation["Accuracy"], Test_Evaluation["Loss"], Test_Evaluation["Top3Acc"], Test_Evaluation["Top5Acc"], Test_Evaluation["Top10Acc"]))
+            print(classification_report(binlabels_true_test, binlabels_pred, target_names=mlb_test.classes_))
+
+
+
+def eval_test(model,
+              test_dataloader,
+              TestCandidatekinaseEmbeddings,
+              TestCandidateKE_to_Kinase,
+              TestKinaseUniProtIDs,
+              mlb_test,
+              args):
+    
+    model.eval()
+        
+    #X, CE, y = next(iter(val_dataloader))
+    X, CE, y = test_dataloader.DE, test_dataloader.ClassEmbedding_with1, test_dataloader.TCI
+    
+    TestCandidateKinases_with1 = torch.from_numpy(np.c_[ TestCandidatekinaseEmbeddings, np.ones(len(TestCandidatekinaseEmbeddings))]).float()
+    
+    X.to(args.DEVICE)
+    TestCandidateKinases_with1.to(args.DEVICE)
+
+    allUniProtIDs = []
+    allprobs = []
+
+    with torch.no_grad():
+
+        pred = model(X)
+
+        logits = torch.matmul(pred, TestCandidateKinases_with1.T)
+        outclassidx = torch.argmax(logits, dim=1) 
+        classes = TestCandidatekinaseEmbeddings[outclassidx]
+        probabilities = softmax(logits, axis =1)
+
+    # get UniProtIDs for predicted classes and return them
+    UniProtIDs =[]
+    for c in classes:
+        UniProtIDs.append(TestCandidateKE_to_Kinase[tuple(c)])
+    UniProtIDs = np.array(UniProtIDs)
+
+    Test_Evaluation, binlabels_pred = get_eval_predictions(UniProtIDs, probabilities, TestKinaseUniProtIDs, y, mlb_test)
+
+    return Test_Evaluation, UniProtIDs, probabilities, binlabels_pred
